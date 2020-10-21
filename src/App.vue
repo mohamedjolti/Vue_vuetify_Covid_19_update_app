ed3d52839abaf019686dd3c77da8bdf9293f04e2
Mohamed JOLTI <mohamedelkholti062@gmail.com>
2020-10-21 14:21:12 +0100

    Vutify covid  19 update

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <v-app>
+      <h1 > Covid 19 update </h1>
+           <Table/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Table from "./components/Table"
+export default {
+  name: 'App',
+  components: {
+    Table
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+      dark
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+  export default {
+
+    mounted(){
+        fetch("https://corona.lmao.ninja/v2/countries").then(response=>response.json()).then((data)=>{
+            console.log(data);
+                    this.desserts=data
+        })
+    } ,
+    data () {
+      return {
+        search: '',
+        headers: [
+         {text:"country",value:"country"},
+         {text:"cases",value:"cases"},
+         {text:"deaths",value:"deaths"},
+         {text:"todayDeaths",value:"todayDeaths"},
+         {text:"recovered",value:"recovered"},
+         {text:"casesPerOneMillion",value:"casesPerOneMillion"},
+         {text:"deathsPerOneMillion",value:"deathsPerOneMillion"},
+         {text:"tests",value:"tests"},
+         {text:"testsPerOneMillion",value:"testsPerOneMillion"},
+
+],
+        desserts: [
+      
+        ],
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
